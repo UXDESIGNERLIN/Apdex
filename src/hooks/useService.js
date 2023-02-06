7a325accef84bf750a3af54b577630101790c9b7
UXDESIGNERLIN <hochialing91@gmail.com>
2023-02-06 12:20:56 +0100

    Constructing useService hook

--- src/hooks/useService.js
+++ src/hooks/useService.js
@@ -1,26 +1,11 @@
-import { useEffect, useMemo, useState } from 'react';
-import hostsCollection from '../utils/hostsCollection';
-import useHostMap from './useHostMap';
-
 const desiredList = 25;
 
-const useService = (data) => {
-
-
+const useService = () => {
   const getTopAppsByHost = (hostname, collecton) => collecton.get(hostname).slice(0, desiredList)
   
-  const addAppToHosts = () => {
-    
-  }
-  
-  const removeAppFromHosts = useMemo(() => (appId) => {
-    // logic to remove an application with higher Apdex
-  }, []);
 
   return {
-    getTopAppsByHost,
-    addAppToHosts,
-    removeAppFromHosts,
+    getTopAppsByHost
   };
 };
 
