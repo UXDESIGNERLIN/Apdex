261e7dce5385bae19fa2627369737f90413226e6
UXDESIGNERLIN <hochialing91@gmail.com>
2023-02-06 09:23:06 +0100

    Refactor hostMap

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -1,28 +1,24 @@
 import useService from "../hooks/useService";
 import data from '../data/host-app-data.json';
-import { useEffect, useState } from "react";
-import hostsCollection from "../utils/hostsCollection";
+import useHostMap from "../hooks/useHostMap";
 
 const HomePage = () => {
-  const [hostMap, setHostMap] = useState(new Map());
   const { getTopAppsByHost } = useService(data);
-  useEffect(() => { 
-    setHostMap(hostsCollection(data));
-  }, []);   
+  const hostMap = useHostMap(data);
 
   const getHostList = () => {
     let hostList = [];
     if (hostMap) {
       for (const hostName of hostMap.keys()) {
         hostList.push(
-          <>
+          <div key={hostName}>
             <h3>{hostName}</h3>
             <ul>
-              {getTopAppsByHost(hostName).slice(0,5).map((item, index) => (
+              {getTopAppsByHost(hostName,hostMap).slice(0,5).map((item, index) => (
                 <li key={index}>{item.name}</li>
               ))}
             </ul>
-          </>
+          </div>
         );
       }
     }
