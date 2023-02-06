4ba3f36cd530fd43848dde6beea1d1111944365a
UXDESIGNERLIN <hochialing91@gmail.com>
2023-02-06 09:21:59 +0100

    Added collection as argument

--- src/hooks/useService.js
+++ src/hooks/useService.js
@@ -1,10 +1,13 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import hostsCollection from '../utils/hostsCollection';
+import useHostMap from './useHostMap';
 
 const desiredList = 25;
 
 const useService = (data) => {
-  const getTopAppsByHost = (hostname) => hostsCollection(data).get(hostname).slice(0, desiredList)
+
+
+  const getTopAppsByHost = (hostname, collecton) => collecton.get(hostname).slice(0, desiredList)
   
   const addAppToHosts = () => {
     
