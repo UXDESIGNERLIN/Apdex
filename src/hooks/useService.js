5944214f7bf1b7e26adc365e3c473a564bab4435
UXDESIGNERLIN <hochialing91@gmail.com>
2023-02-06 07:13:23 +0100

    Offered given hostname

--- src/hooks/useService.js
+++ src/hooks/useService.js
@@ -1,8 +1,10 @@
 import { useMemo } from 'react';
 import hostsCollection from '../utils/hostsCollection';
 
+const desiredList = 25;
+
 const useService = (data) => {
-  const getTopAppsByHost =  hostsCollection(data);
+  const getTopAppsByHost = (hostname) => hostsCollection(data).get(hostname).slice(0, desiredList)
   
   const addAppToHosts = () => {
     
