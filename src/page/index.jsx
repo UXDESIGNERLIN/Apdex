2b26ce34922082c17793ffb4ce8f49ae97ccb56d
UXDESIGNERLIN <hochialing91@gmail.com>
2023-02-06 08:21:15 +0100

    rerendered the list for all of the host map

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -1,22 +1,37 @@
 import useService from "../hooks/useService";
 import data from '../data/host-app-data.json';
 import { useEffect, useState } from "react";
+import hostsCollection from "../utils/hostsCollection";
 
 const HomePage = () => {
-  const [hosts, setHosts] = useState();
+  const [hostMap, setHostMap] = useState(new Map());
   const { getTopAppsByHost } = useService(data);
   useEffect(() => { 
-    setHosts(getTopAppsByHost(testHost));   
-  }, []); 
-  const testHost = '95b346a0-17f4.abbigail.name'
+    setHostMap(hostsCollection(data));
+  }, []);   
+
+  const getHostList = () => {
+    let hostList = [];
+    if (hostMap) {
+      for (const hostName of hostMap.keys()) {
+        hostList.push(
+          <>
+            <h3>{hostName}</h3>
+            <ul>
+              {getTopAppsByHost(hostName).slice(0,5).map((item, index) => (
+                <li key={index}>{item.name}</li>
+              ))}
+            </ul>
+          </>
+        );
+      }
+    }
+    return hostList;
+  }
   
   return(
     <>
-    {hosts &&
-      <ul>
-        {hosts.map((item, index) => <li key={index}>{item.name}</li>)}
-      </ul>
-    }
+     {getHostList()}
     </>
   )
 }
