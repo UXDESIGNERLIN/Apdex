437a32f72e8c7797c06fadd888b59af807e54898
UXDESIGNERLIN <hochialing91@gmail.com>
2023-02-07 12:57:57 +0100

    Clean up code

---
+++ src/__mocks__/app.fixture.js
@@ -0,0 +1,9 @@
+const MOCK_APP = {
+  name:"testing",
+  contributors:["Mrs. Claudine Gislason","Gerson Leannon","Lawson Jast"],
+  version:1,
+  apdex:100,
+  host:["e7bf58af-f0be.dallas.biz","7e6272f7-098e.dakota.biz","1d717554-bf17.sydnie.name","e0419f48-6a5a.craig.info","2b4cfcf7-81d5.kelli.org", "test.org"]
+}
+
+export default MOCK_APP;
